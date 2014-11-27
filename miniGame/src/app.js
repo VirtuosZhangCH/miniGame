a3b935ec7c987e97e759f68d8ba3b7406720b7aa
zhangchi <zhangchi@virtuos.com.cn>
2014-11-27 14:40:25 +0800

    catbar

--- miniGame/src/app.js
+++ miniGame/src/app.js
@@ -18,7 +18,10 @@ var HelloWorldLayer = cc.Layer.extend({
         this.addChild(bg,0);
         bg.x=bg.height/2-90;//>>1;
         bg.y=bg.width/2+90;//>>1;
-        var testbt=new ChatBarLayer();
+        this.chatBarLayer=new ChatBarLayer();
+        this.addChild(this.chatBarLayer);
+        this.chatBarLayer.x=320;
+        this.chatBarLayer.y=80;
         //this.addChild(testbt,1);
         // add a "close" icon to exit the progress. it's an autorelease object
        /* var closeItem = new cc.MenuItemImage(
--- miniGame/src/miniGameComponent/view/elementsViews/ChatBarLayer.js
+++ miniGame/src/miniGameComponent/view/elementsViews/ChatBarLayer.js
@@ -14,27 +14,58 @@ var ChatBarLayer=cc.Sprite.extend({
 
     },
 
+    onEnter:function () {
+        this._super();
+
+        // add CCTextFieldTTF
+        var winSize = cc.director.getWinSize();
+
+    },
+
     layout:function()
     {
+        this.SpinBt.x=247;
+        this.SpinBt.y=-10;
 
+        //this._tf.x=50;
      },
 
     initButton:function()
     {
+        var Spin=new cc.Sprite("#ms_button_fs.png");
+        var Spin2=new cc.Sprite("#ms_button_fs.png");
+        var Spin3=new cc.Sprite("#ms_button_fs.png");
 
+        this.SpinBt=new cc.MenuItemSprite(Spin,Spin2,Spin3,this.onCallBack,this);
+
+        var greenBg=new cc.Sprite("#ms_button_left.png");
+
+        this._tf=new cc.TextFieldTTF("12345", "Microsoft Yahei", 40);
+        this._tf.setHorizontalAlignment(cc.TEXT_ALIGNMENT_CENTER);
+        this._tf.color=0;
+        this._tf.setDelegate(this);
+
+       // this._trackNode =this._tf;
+
+        this.addChild(this.SpinBt);
+        this.addChild(greenBg);
+        this.addChild(this._tf);
+
+        greenBg.x=-234;
+        greenBg.y=-10;
     },
 
     onCallBack:function()
     {
         //test
-
+        this._tf.visible=false;
         //cc.eventManager.dispatchCustomEvent(SpinResponseEvent.SPIN_RESPONSE,"KKKOOO");
         this.sigOnClick.dispatch();
     },
 
     initBg:function()
     {
-        var Spin=new cc.Sprite("#bg.png");
-        this.addChild(Spin);
+        var bg=new cc.Sprite("#ms_bottombg.png");
+        this.addChild(bg);
     }
 })
