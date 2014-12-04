a0d9adee3cb8f81833c2f0bdd6c00abe8ba44675
zhangchi <zhangchi@virtuos.com.cn>
2014-12-04 17:49:16 +0800

    chat unit

--- miniGame/src/miniGameComponent/view/elements/ChatUnit.js
+++ miniGame/src/miniGameComponent/view/elements/ChatUnit.js
@@ -5,13 +5,13 @@ var ChatUnit=cc.Sprite.extend({
     testBt:null,
     sigOnClick:new Signal(),
     _status:0,
-    ctor:function()
+    _pic:null,
+    ctor:function($picName,$status)
     {
         cc.Sprite.prototype.ctor.call(this);
-
+        this._status=$status;
         cc.defineGetterSetter(this,"status",this.getStatus,this.setStatus);
         this.initBg();
-        this.initButton();
         this.layout();
     },
 
@@ -22,93 +22,15 @@ var ChatUnit=cc.Sprite.extend({
 
     setStatus:function(value)
     {
-        this._status=value
+        this._status=value;
     },
 
     layout:function()
     {
-       // this.SpinBt.x=247;
-        //this.SpinBt.y=-10;
-
-        //this._tf.x=50;
+        this.topbg.y=20;
+        this.botbg.y=20;
      },
 
-    initButton:function()
-    {
-        var Spin=new cc.Sprite("#ms_button_fs.png");
-        var Spin2=new cc.Sprite("#ms_button_fs.png");
-        var Spin3=new cc.Sprite("#ms_button_fs.png");
-
-        var SpinBt=new cc.MenuItemSprite(Spin,Spin2,Spin3,this.onCallBack,this);
-        SpinBt.retain();
-        var greenBg=new cc.Sprite("#ms_button_left.png");
-        this.infoMenu=new cc.Menu(SpinBt);
-        this.infoMenu.retain();
-
-        //this.textF=new cc.TextFieldTTF("Click here");
-        //this.textF.setDelegate(this);
-        //this.textF.setColor(new cc.Color(0,0,0,0));
-        this._userName = new cc.EditBox(cc.size(200, 54), new cc.Scale9Sprite("qp_bg_mid.png"));
-        //this._userName.setHorizontalAlignment(cc.TEXT_ALIGNMENT_CENTER);
-        this._userName.setFontColor(new cc.Color(0,0,0,0));
-        this._userName.setFontSize(20);
-        this._userName.setPlaceHolder("Enter UserName");
-        this._userName.setDelegate(this);
-        this._userName.setMaxLength(20);
-
-        /*this.te=new cc.TextFieldTTF("1234", "Microsoft Yahei", 50);
-        //te.setDelegate(this);
-        this.te.setHorizontalAlignment(cc.TEXT_ALIGNMENT_CENTER);
-        this.te.setTextColor(new cc.Color(0,0,0,255));
-        this.te.attachWithIME();
-        this.te.x=100;
-        this.te.y=20;
-
-        //touch
-        cc.eventManager.addListener({
-            event: cc.EventListener.MOUSE,
-            onMouseUp: this.onMouseUp.bind(this)
-        }, this);*/
-
-        this.addChild(greenBg);
-        this.addChild(this.infoMenu);
-        this.addChild(this._userName);
-
-        //this.addChild(this.te);
-
-        this.infoMenu.x=240;
-        this.infoMenu.y=-10;
-        greenBg.x=-234
-        greenBg.y=-10;
-    },
-
-    /*onMouseUp:function (event) {
-        var target = event.getCurrentTarget();
-        var point = event.getLocationInView();
-        //this.te.alpha=0;
-        // decide the trackNode is clicked.
-        cc.log("KeyboardNotificationLayer:clickedAt(" + point.x + "," + point.y + ")");
-
-        var rect = textInputGetRect(this.te);
-        cc.log("KeyboardNotificationLayer:TrackNode at(origin:" + rect.x + "," + rect.y
-            + ", size:" + rect.width + "," + rect.height + ")");
-
-        target.onClickTrackNode(cc.rectContainsPoint(rect, point));
-        cc.log("----------------------------------");
-    },
-
-    onClickTrackNode:function (clicked) {
-        var textField = this.te;
-        //if (clicked) {
-            // TextFieldTTFTest be clicked
-            cc.log("TextFieldTTFDefaultTest:CCTextFieldTTF attachWithIME");
-            textField.attachWithIME();
-       // } else {
-            // TextFieldTTFTest not be clicked
-           // cc.log("TextFieldTTFDefaultTest:CCTextFieldTTF detachWithIME");
-           // textField.detachWithIME();
-      //  }
-    },*/
 
     onCallBack:function()
     {
@@ -120,14 +42,12 @@ var ChatUnit=cc.Sprite.extend({
 
     initBg:function()
     {
-        var bg=new cc.Sprite("#ms_bottombg.png");
-        this.addChild(bg);
+        this.midbg=new cc.Sprite("#qp2_bg_mid.png");
+        this.topbg=new cc.Sprite("#qp2_bg_top.png");
+        this.botbg=new cc.Sprite("#qp2_bg_bottom.png");
+
+        this.addChild(this.midbg);
+        this.addChild(this.topbg);
+        this.addChild(this.botbg);
     }
 })
-
-var textInputGetRect = function (node) {
-    var rc = cc.rect(node.x, node.y, node.width, node.height);
-    rc.x -= rc.width / 2;
-    rc.y -= rc.height / 2;
-    return rc;
-};
