e723e467e34debf1cd7c48c17f2f17c1eab6ba48
zhangchi <zhangchi@virtuos.com.cn>
2014-12-09 14:52:05 +0800

    TODO status of chat unit

--- miniGame/src/miniGameComponent/view/elements/ChatUnit.js
+++ miniGame/src/miniGameComponent/view/elements/ChatUnit.js
@@ -4,24 +4,36 @@
 var ChatUnit=cc.Sprite.extend({
     testBt:null,
     sigOnClick:new Signal(),
+    _type:0,
     _status:0,
-    ctor:function($picName,$status)
+    ctor:function($picName,$type)
     {
         cc.Sprite.prototype.ctor.call(this);
-        this._status=$status;
-        cc.defineGetterSetter(this,"status",this.getStatus,this.setStatus);
-        this.initBg($picName,$status);
+        this._type=$type;
+        cc.defineGetterSetter(this,"type",this.gettype,this.settype);
+        cc.defineGetterSetter(this,"status",this.getstatus,this.setstatus);
+        this.initBg($picName,$type);
         this.layout();
     },
 
-    getStatus:function()
+    getstatus:function()
     {
         return this._status;
     },
 
-    setStatus:function(value)
+    setstatus:function(value)
     {
         this._status=value;
+    },
+
+    gettype:function()
+    {
+        return this._type;
+    },
+
+    settype:function(value)
+    {
+        this._type=value;
     },
 
     setText:function(value)
@@ -37,7 +49,7 @@ var ChatUnit=cc.Sprite.extend({
 
         this._chatGroup.x=230;
 
-        if( this._status==0)
+        if( this._type==0)
         {
             this.botbg.x=25;
             this.pic.x=
@@ -53,10 +65,10 @@ var ChatUnit=cc.Sprite.extend({
         this.sigOnClick.dispatch();
     },
 
-    initBg:function($picName,$status)
+    initBg:function($picName,$type)
     {
         this._chatGroup=new cc.Sprite();
-        this._status=$status;
+        this._type=$type;
         this._whiteCircle=new cc.Sprite("#tx_top_bg.png");
 
         this._desTf=new cc.LabelTTF("这就是异常","Microsoft Yahei",20);
@@ -68,7 +80,7 @@ var ChatUnit=cc.Sprite.extend({
         this.addChild(this._whiteCircle);
         this.addChild(this._chatGroup);
 
-        if($status==0){
+        if($type==0){
             this.midbg = new cc.Sprite("#qp2_bg_mid.png");
             this.topbg = new cc.Sprite("#qp2_bg_top.png");
             this.botbg = new cc.Sprite("#qp2_bg_bottom.png");
@@ -81,7 +93,6 @@ var ChatUnit=cc.Sprite.extend({
         this._chatGroup.addChild(this.topbg);
         this._chatGroup.addChild(this.botbg);
         this._chatGroup.addChild(this.midbg);
-
         this._chatGroup.addChild(this._desTf);
     }
 })
